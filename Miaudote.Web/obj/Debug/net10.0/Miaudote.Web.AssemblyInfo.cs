25c0e6f693d580b537d13dec5b83d561c60e563d
ane-noelle <anenoelle60@gmail.com>
2026-07-28 12:00:35 -0300

    Finaliza página inicial estática no MVC

--- Miaudote.Web/obj/Debug/net10.0/Miaudote.Web.AssemblyInfo.cs
+++ Miaudote.Web/obj/Debug/net10.0/Miaudote.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Miaudote.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc5e1d6fca3aee906dd928f66faf4cdc75655ab2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd95ecd4c25c0754c7930061681570d2f9dfca75")]
 [assembly: System.Reflection.AssemblyProductAttribute("Miaudote.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Miaudote.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
