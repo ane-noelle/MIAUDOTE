9fa3a3e804b093c3de3822c7f47020a45f0487b7
ane-noelle <anenoelle60@gmail.com>
2026-08-20 12:10:32 -0300

    Estabiliza projeto e prepara consulta de animais

--- Miaudote.Web/obj/Debug/net10.0/Miaudote.Web.AssemblyInfo.cs
+++ Miaudote.Web/obj/Debug/net10.0/Miaudote.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Miaudote.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e564fa5b6c5011d000a2eac2294920327941723b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7fec269cfa06be3f542c1a6596223f77264621b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Miaudote.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Miaudote.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
