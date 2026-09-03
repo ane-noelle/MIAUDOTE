60684f4c45b53f158683bbf890c30330f7284a50
ane-noelle <anenoelle60@gmail.com>
2026-09-03 11:03:44 -0300

    Implementa aprovacao e recusa de solicitacoes

--- Miaudote.Web/Controllers/AdocaoController.cs
+++ Miaudote.Web/Controllers/AdocaoController.cs
@@ -2,6 +2,7 @@ using Miaudote.Web.Data;
 using Miaudote.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Miaudote.Web.Enums;
 
 namespace Miaudote.Web.Controllers;
 
@@ -55,33 +56,65 @@ public class AdocaoController : Controller
             return View(solicitacao);
         }
 
+        solicitacao.Id = 0;
         solicitacao.DataSolicitacao = DateTime.Now;
 
-        solicitacao.Id = 0;
-
-        await _contexto.SolicitacoesAdocao
-            .AddAsync(solicitacao);
+        await _contexto.SolicitacoesAdocao.AddAsync(solicitacao);
 
         await _contexto.SaveChangesAsync();
 
         TempData["Sucesso"] =
             "Solicitação enviada com sucesso!";
 
-        TempData["Sucesso"] =
-    "Solicitação enviada com sucesso!";
-
-return RedirectToAction(
-    nameof(Solicitar),
-    new { id = solicitacao.AnimalId });
+        return RedirectToAction(
+            nameof(Solicitar),
+            new { id = solicitacao.AnimalId });
     }
-    public async Task<IActionResult> Solicitacoes()
-{
-    var solicitacoes = await _contexto.SolicitacoesAdocao
-        .AsNoTracking()
-        .Include(s => s.Animal)
-        .OrderByDescending(s => s.DataSolicitacao)
-        .ToListAsync();
 
-    return View(solicitacoes);
-}
+    public async Task<IActionResult> Solicitacoes()
+    {
+        var solicitacoes = await _contexto.SolicitacoesAdocao
+            .AsNoTracking()
+            .Include(s => s.Animal)
+            .OrderByDescending(s => s.DataSolicitacao)
+            .ToListAsync();
+
+        return View(solicitacoes);
+    }
+
+    public async Task<IActionResult> Aprovar(int id)
+    {
+        var solicitacao = await _contexto.SolicitacoesAdocao
+            .FirstOrDefaultAsync(s => s.Id == id);
+
+        if (solicitacao == null)
+        {
+            return NotFound();
+        }
+
+        solicitacao.Status = StatusSolicitacao.Aprovada;
+        solicitacao.DataAtualizacao = DateTime.Now;
+
+        await _contexto.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Solicitacoes));
+    }
+
+    public async Task<IActionResult> Recusar(int id)
+    {
+        var solicitacao = await _contexto.SolicitacoesAdocao
+            .FirstOrDefaultAsync(s => s.Id == id);
+
+        if (solicitacao == null)
+        {
+            return NotFound();
+        }
+
+        solicitacao.Status = StatusSolicitacao.Recusada;
+        solicitacao.DataAtualizacao = DateTime.Now;
+
+        await _contexto.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Solicitacoes));
+    }
 }
